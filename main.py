226f983a47f38a26eafddb4db1b88744975624b2
laszlogulyas.jr@gmail.com <laszlogulyas.jr@gmail.com>
2022-04-30 16:55:51 +0200

    Update main with closing log

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from application.controller.record_editor_controller import RecordEditorController
 
-if __name__ == '__main__':
-    print('Application starting...')
+if __name__ == "__main__":
+    print("Application starting...")
     main_controller = RecordEditorController()
     main_controller.create_view()
+    print("Application closed!")
 
